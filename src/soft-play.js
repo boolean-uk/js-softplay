b5377be6352b418580f1d98e67ccfb7f27ce73af
usamalodhi <lodhius@gmail.com>
2021-12-16 19:47:46 +0000

    soft play submission - comments removed

--- src/soft-play.js
+++ src/soft-play.js
@@ -19,12 +19,6 @@ function enter(numAdults, numChildren) {
   }
 }
 
-// it("Adult cannot leave when adults 1 and children 1", function() {
-//   reset()
-//   enter(1,1)
-//   expect(leave(1,0)).toBeFalse()
-//   expect(occupancy()).toEqual({adults: 1, children: 1})
-// })
 function leave(numAdults, numChildren) {
   if (adults === 1 && children >= 1) {
     return false;
@@ -39,17 +33,9 @@ function leave(numAdults, numChildren) {
   children -= numChildren;
   return true;
 }
-
-console.log(enter(1, 0));
-console.log(leave(3, 0));
-console.log(adults);
-console.log(children);
-console.log(counter);
-
 function occupancy() {
   return { adults: adults, children: children };
 }
-console.log(occupancy);
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
