bdc54f44fe2e5c00a71c6e88b7096d09fdcd5497
omar <omaralsoyouf@gmail.com>
2021-12-16 14:02:38 +0000

    1st submission - Omar

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,12 +1,12 @@
 // do not change these lines
-
-function reset () {
-  adults = 0
-  children = 0
+//demo
+function reset() {
+  adults = 0;
+  children = 0;
 }
 
-let adults = 0
-let children = 0
+let adults = 0;
+let children = 0;
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
@@ -16,5 +16,5 @@ module.exports = {
   enter: undefined,
   leave: undefined,
   occupancy: undefined,
-  reset: reset
-}
+  reset: reset,
+};
