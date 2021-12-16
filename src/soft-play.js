6c3332318f3d4ab2f9f5f1531cf6cbef8b9f8b16
omar <omaralsoyouf@gmail.com>
2021-12-16 18:37:31 +0000

    Omar Submission

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,47 +1,47 @@
 // do not change these lines
-function reset() {
-  adults = 0;
-  children = 0;
+function reset () {
+  adults = 0
+  children = 0
 }
 
-let adults = 0;
-let children = 0;
+let adults = 0
+let children = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
-function enter(adultsNum, childrenNum) {
+function enter (adultsNum, childrenNum) {
   if (adultsNum >= childrenNum) {
-    adults += adultsNum;
-    children += childrenNum;
-    return true;
+    adults += adultsNum
+    children += childrenNum
+    return true
   } else {
-    return false;
+    return false
   }
 }
-function leave(adultsNum, childrenNum) {
+function leave (adultsNum, childrenNum) {
   if (adultsNum === 0 && childrenNum > 0) {
-    return false;
+    return false
   } else if (adults - adultsNum === 0 && children - childrenNum !== 0) {
-    return false;
+    return false
   } else if (adults === 0 || children === 0) {
-    return false;
+    return false
   } else if (adultsNum > adults || childrenNum > children) {
-    return false;
+    return false
   } else {
-    adults -= adultsNum;
-    children -= childrenNum;
-    return true;
+    adults -= adultsNum
+    children -= childrenNum
+    return true
   }
 }
 
-let occ = {
+const occ = {
   adults: 0,
-  children: 0,
-};
-function occupancy() {
-  occ.adults = adults;
-  occ.children = children;
-  return occ;
+  children: 0
+}
+function occupancy () {
+  occ.adults = adults
+  occ.children = children
+  return occ
 }
 
 // TODO: Change the undefined values below to the name of your functions
@@ -49,5 +49,5 @@ module.exports = {
   enter: enter,
   leave: leave,
   occupancy: occupancy,
-  reset: reset,
-};
+  reset: reset
+}
