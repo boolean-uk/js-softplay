df2e8f22fca442e46ecc3422bbf0675c806a83cf
Ben Corbin <ben@corbin.digital>
2021-12-16 16:08:14 +0000

    Working with Usama

--- src/soft-play.js
+++ src/soft-play.js
@@ -4,17 +4,37 @@ function reset () {
   adults = 0
   children = 0
 }
-
+counter = 0
 let adults = 0
 let children = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 
+function enter(numChildren, numAdults) {
+  if (numChildren ===  numAdults) {
+  adults = adults + numAdults;
+  children = children + numChildren;
+  counter = counter + numChildren + numAdults;
+  return true;
+  }
+}
+
+function leave(numChildren, numAdults) {
+    if (numChildren < numAdults) return false
+}
+
+
+
+function occupancy() {
+  return { adults: adults, children: children}
+}
+
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined,
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy,
   reset: reset
 }
