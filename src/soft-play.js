fe2b56e7fef86803319ad8dde2895d426e816c04
marianphillips <79763160+marianphillips@users.noreply.github.com>
2021-12-16 21:42:43 +0000

    trying to get ESLint formatting

--- src/soft-play.js
+++ src/soft-play.js
@@ -2,61 +2,59 @@
 function reset () {
   adults = 0
   children = 0
-  adultsTotal = 0;
-  childrenTotal = 0;
+  adultsTotal = 0
+  childrenTotal = 0
 }
 
 let adults = 0
 let children = 0
 
-let adultsTotal = 0;
-let childrenTotal = 0;
+let adultsTotal = 0
+let childrenTotal = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
-function occupancy() {
+function occupancy () {
   return {
     adults: adults,
-    children: children,
-  };
+    children: children
+  }
 }
 
-function total() {
+function total () {
   return {
     adults: adultsTotal,
-    children: childrenTotal,
-  };
-}
-
-
-function enter(numAdult, numChildren) {
-  if (numAdult < numChildren) {
-    return false;
+    children: childrenTotal
   }
-  adults += numAdult;
-  children += numChildren;
-  adultsTotal += numAdult;
-  childrenTotal += numChildren;
-  return true;
 }
-function leave(numAdult, numChildren) {
+
+function enter (numAdult, numChildren) {
+  if (numAdult < numChildren) {
+    return false
+  }
+  adults += numAdult
+  children += numChildren
+  adultsTotal += numAdult
+  childrenTotal += numChildren
+  return true
+}
+function leave (numAdult, numChildren) {
   if (numAdult === 0) {
-    return false;
+    return false
   }
   if (adults === 1 && numAdult >= 1) {
-    return false;
+    return false
   }
   if (adults - numAdult < children - numChildren) {
-    return false;
+    return false
   }
   if (adults < numAdult || children < numChildren) {
-    return false;
+    return false
   }
-  adults -= numAdult;
-  children -= numChildren;
-  return true;
+  adults -= numAdult
+  children -= numChildren
+  return true
 }
-
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
@@ -64,5 +62,5 @@ module.exports = {
   leave: leave,
   occupancy: occupancy,
   reset: reset,
-  total: total,
-};
+  total: total
+}
