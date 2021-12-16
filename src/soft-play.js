f26b0bbbdce41d1aca1a04d4e5b6c820cc48136e
Paul Smart <pdsmart6@gmail.com>
2021-12-16 16:36:59 +0000

    test two passed

--- src/soft-play.js
+++ src/soft-play.js
@@ -10,31 +10,31 @@ let children = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
-function enter(numChildren, numAdults) {
-if (numChildren > numAdults)
-
-
+function enter (numChildren, numAdults) {
+  if (numChildren > numAdults) {
+    return false
+  }
+  adults += numAdults
+  children += numChildren
+  return true
 }
-  return   
- {
-return false
-}
 
-
-function leave(numChildren, numAdults) {
+function leave (numChildren, numAdults) {
 
 }
 
-function occupancy() {
-  if 
+function occupancy () {
+  const object = {}
+  object.adults = adults
+  object.children = children
+  return object
 }
-
 
 // TODO: Change the undefined values below to the name of your functions
-module.exports
- = {
-  enter: enter,
-  leave: leave,
-  occupancy: occupancy,
-  reset: reset
-}
+module.exports =
+ {
+   enter: enter,
+   leave: leave,
+   occupancy: occupancy,
+   reset: reset
+ }
