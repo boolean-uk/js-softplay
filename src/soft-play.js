9d76a66962442384e9cd190708d55487452575eb
usamalodhi <lodhius@gmail.com>
2021-12-16 19:31:13 +0000

    soft play submission

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,20 +1,60 @@
 // do not change these lines
 
-function reset () {
-  adults = 0
-  children = 0
+function reset() {
+  adults = 0;
+  children = 0;
+}
+counter = 0;
+let adults = 0;
+let children = 0;
+
+function enter(numAdults, numChildren) {
+  if (numAdults === 0) {
+    return false;
+  }
+  if (numAdults >= numChildren) {
+    adults += numAdults;
+    children += numChildren;
+    return true;
+  }
 }
 
-let adults = 0
-let children = 0
+// it("Adult cannot leave when adults 1 and children 1", function() {
+//   reset()
+//   enter(1,1)
+//   expect(leave(1,0)).toBeFalse()
+//   expect(occupancy()).toEqual({adults: 1, children: 1})
+// })
+function leave(numAdults, numChildren) {
+  if (adults === 1 && children >= 1) {
+    return false;
+  }
+  if (adults - numAdults < children - numChildren) {
+    return false;
+  }
+  if (adults < numAdults || children < numChildren) {
+    return false;
+  }
+  adults -= numAdults;
+  children -= numChildren;
+  return true;
+}
 
-// TODO: Write your functions in the below section. Your functions should update
-// the adults and children variables defined above.
+console.log(enter(1, 0));
+console.log(leave(3, 0));
+console.log(adults);
+console.log(children);
+console.log(counter);
+
+function occupancy() {
+  return { adults: adults, children: children };
+}
+console.log(occupancy);
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined,
-  reset: reset
-}
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy,
+  reset: reset,
+};
