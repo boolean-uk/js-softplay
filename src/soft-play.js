a7fff7ceab19a1814a7f5e7e2cdc7732e0ad8311
Ben Corbin <ben@corbin.digital>
2021-12-17 10:24:52 +0000

    Updated to pass git tests

--- src/soft-play.js
+++ src/soft-play.js
@@ -8,24 +8,17 @@ counter = 0
 let adults = 0
 let children = 0
 
-
 function enter(numAdults, numChildren) {
-   if (numAdults === 0) {
-    return false
+  if (numAdults === 0) {
+  return false
   }
   if (numAdults >= numChildren) {
-    adults+=numAdults; 
-    children+=numChildren;
+    adults += numAdults; 
+    children += numChildren
     return true;
   }
   }
 
-// it("Adult cannot leave when adults 1 and children 1", function() {
-//   reset()
-//   enter(1,1)
-//   expect(leave(1,0)).toBeFalse()
-//   expect(occupancy()).toEqual({adults: 1, children: 1})
-// })
 function leave(numAdults, numChildren) {
     if (adults ===1 && children >=1) {
   return false;
@@ -36,23 +29,14 @@ function leave(numAdults, numChildren) {
     if ((adults < numAdults) || (children < numChildren)) {
       return false;
     }
-    adults-=numAdults; 
-    children-=numChildren;
-    return true;
-    
+    adults -= numAdults; 
+    children -= numChildren;
+    return true; 
 }
-  
-    
-console.log(enter(1,0))
-console.log(leave(3,0))
-console.log(adults)
-console.log(children)
-console.log(counter) 
 
 function occupancy() {
   return { adults: adults, children: children}
 }
-console.log(occupancy)
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
