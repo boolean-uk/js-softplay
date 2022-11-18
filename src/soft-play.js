9283859f233ffb7123eacc3f26e2d93bb2b00a55
Kat <katdemey@outlook.com>
2022-11-18 23:51:56 +0100

    completed core whith successful console logs, but can't make heads or tails of why the npm tests are failing so that'll need some further investigation

--- src/soft-play.js
+++ src/soft-play.js
@@ -6,9 +6,60 @@ let children = 0
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
+
+const totalGuests = {
+  adults: 0,
+  children: 0
+}
+
+function occupancy() {
+  return {
+    adult: adults,
+    children: children,
+  }
+}
+
+console.log(occupancy())
+
+// phase 2
+  
+    
+function enter(enterAdult, enterKids) {
+//conditions: 1 adult per child minimum
+  if (enterAdult >= enterKids) {
+    totalGuests.adults += enterAdult
+    totalGuests.children += enterKids
+    adults += enterAdult
+    children += enterKids
+    return true
+    } else {
+      return false
+    }}
+
+ console.log(enter(2, 1))
+ console.log(totalGuests)
+
+
+// // phase 3
+ function leave(numAdult, numChild) {
+  if (numChild > numAdult || (children - numChild) > (adults - numAdult)) {
+    return false
+  } else {
+    totalGuests.adults -= numAdult
+    totalGuests.children -= numChild
+    adults -= numAdult
+    children -= numChild
+    return true}
+}
+ 
+ console.log(leave (2, 1))
+ console.log(totalGuests) 
+
+
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+  enter: enter(),
+  leave: leave(),
+  occupancy: occupancy()
 }
