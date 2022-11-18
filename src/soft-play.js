9bbdd7e5ec872b611ab63b1ba8f642c47711465a
Ahmed Missouri <ahmedmissouri@icloud.com>
2022-11-18 14:49:38 +0000

    Finished extension

--- src/soft-play.js
+++ src/soft-play.js
@@ -6,22 +6,24 @@ let children = 0
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
+const totalObject = {
+  adults: 0,
+  children: 0
+}
+
 function occupancy() {
   return {
     adults: adults,
     children: children,
   }
 }
-let totalAdults = 0
-let totalChildren = 0
 
 function entry(newAdults, newChildren) {
   if(newAdults >= newChildren) {
-    console.log(newAdults, newChildren)
+    totalObject.adults += newAdults
+    totalObject.children += newChildren
     adults += newAdults
     children += newChildren
-    totalAdults += newAdults
-    totalChildren += newChildren
     return true
   } 
   return false
@@ -36,18 +38,7 @@ function exit(newAdults, newChildren){
   return true
 }
 
-console.log(totalAdults)
-console.log(totalChildren)
-
-function total(adults, children) {
-  const totalObject = {
-    adults: 0,
-    children: 0,
-  }
-  console.log(entry())
-  total.adults = total.adults + entry()
-  total.children = total.children + entry()
-
+function total() {
   return totalObject
 }
 
