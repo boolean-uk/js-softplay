96daeb0be6a9daecabeedeb5239eade03e2c112a
MaybeFreak <michaelpope97@gmail.com>
2022-11-18 16:53:14 +0100

    made the smallest most meaningless change imaginable

--- src/soft-play.js
+++ src/soft-play.js
@@ -31,7 +31,7 @@ function leave(numAdults, numChildren) {
     console.log('More are leaving than there should be inside!!!')
     return false
   } else if (numAdults < numChildren) {
-    console.log('Every child leaving has to be accompanied by atleast 1 adult.')
+    console.log('Children leaving need to be accompanied by atleast 1 adult.')
     return false
   } else if (adults - numAdults < children - numChildren) {
     console.log('Number of children would be greater than number of adults')
@@ -48,14 +48,12 @@ function totalAdd(numAdults, numChildren) {
   totalEntered.children += numChildren
 }
 
-function totalDisplay() {
-  return totalEntered
-}
+const total = () => totalEntered
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
   enter: enter,
   leave: leave,
   occupancy: checkOccupancy,
-  total: totalDisplay
+  total: total
 }
