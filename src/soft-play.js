07546c2b3bc625108cf7424ecd72b16daa8399d3
benji <benjipictor@gmail.com>
2022-11-22 16:55:38 +0000

    core exercise

--- src/soft-play.js
+++ src/soft-play.js
@@ -4,43 +4,54 @@ let children = 0
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 // Start with the occupancy function.
-function occupancy() {
-return totalOcupancy = ({
-  adults, children})
-}
 
-function enter(numAdults, numChildren){
-  if (numChildren > numAdults) {
-    return false
+
+function occupancy() {
+  return ({
+    adults, children})
   }
-  else if (numChildren <= numAdults) {
+  // currentOcupancy = occupancy (1,2)
+  function enter(numAdults, numChildren){
+    // const allCildrenAcompanied = numChildren <= numAdults
+    // const childNotAcompanied = numChildren > numAdults
+    if (numChildren > numAdults) {
+      return false
+    }
+    else if (numChildren <= numAdults) {
     adults += numAdults
     children += numChildren
     // console.log(`number of adults: ${adults}, numberof children: ${children} `)
     return true   
   }
 }
-
+// enter(10, 10)
 function leave(numAdults, numChildren){
-  if (numChildren > numAdults || numAdults > adults || numChildren > children || adults === 0 || children === 0 ) {
+  const adultIsLargerThanChildren = numAdults < numChildren
+  const adultsLeavingIsMoreThanBalance = numAdults > adults
+  let adultOcupancy = adults
+  let childOcupancy = children
+  if (adultIsLargerThanChildren){
     return false
   }
-  else if (children <= 2 && numAdults > numChildren) {
+  else if(adultsLeavingIsMoreThanBalance) {
     return false
   }
-  else if (numChildren <= numAdults) {
+  adultOcupancy -= numAdults
+  childOcupancy -= numChildren
+  if (adultOcupancy < childOcupancy) {
+    return false
+  }
+  else {
     adults -= numAdults
-    children -= numChildren
-    // console.log(`number of adults: ${adults}, numberof children: ${children} `)
-    return true   
+    children -= numChildren  
+    return true
   }
 }
-console.log(occupancy())
-// enter()
-console.log(occupancy())
-// leave()
-console.log(occupancy())    
-
+console.log(leave(0,0))
+console.log(leave(1,0))
+console.log(leave(1,1))
+console.log(leave(1,2))
+console.log(leave(2,1))
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
