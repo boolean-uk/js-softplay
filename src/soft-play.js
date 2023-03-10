1edc4c61bc9d254499860cea22260790e133be02
carolarruda <carolinacalouroarruda@gmail.com>
2023-03-10 17:56:12 +0100

    Updated core file, test doesn't work

--- src/soft-play.js
+++ src/soft-play.js
@@ -2,43 +2,53 @@
 let adults = 0
 let children = 0
 
-// TODO: Write your functions in the below section. 
+// TODO: Write your functions in the below section.
 //our functions should update
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
-//first we need a function to register who enters
+//Function 1 Enter
 function enter(numAdults, numChildren) {
   adults += numAdults
   children += numChildren
-  console.log("Number of Adults", adults)
-  console.log("Number of kids", children)
+  console.log('Number of Adults', adults)
+  console.log('Number of kids', children)
   if (numAdults < numChildren) {
     return false
   } else {
     return true
   }
-
 }
-console.log(enter(4,2))
+console.log(enter(, ))
+//Function 2 Leave center
 
 function leave(numAdults, numChildren) {
   adults -= numAdults
   children -= numChildren
-  console.log("Number of Adults leaving", adults)
-  console.log("Number of kid leaving", children)
-  if (adults <= children) {
-    return false
-  }if (numAdults >= numChildren) {
+  console.log('Number of Adults in the building', adults)
+  console.log('Number of kid in the building', children)
+  if (adults <= children || numAdults <= numChildren) {
     return false
   } else {
     return true
   }
 }
-console.log(leave(0, 1))
+
+console.log(leave(, ))
+
+//Function Ocuupancy
+
+function occupancy() {
+  let counter = 0
+  counter = { adults: adults, children: children }
+  return counter
+}
+console.log(occupancy())
+
+//
 // TODO: Change the undefined values below to the name of your functions
-// module.exports = {
-//   enter: enter,
-//   leave: undefined,
-//   occupancy: undefined
-// }
+module.exports = {
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy
+}
