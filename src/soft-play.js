b38e56f6cab41b3c29a4ec54e3dd12a47d5d5731
Ian Grantham <dev.iangrantham@gmail.com>
2023-03-10 16:10:03 +0000

    initial plan outlined

--- src/soft-play.js
+++ src/soft-play.js
@@ -6,6 +6,31 @@ let children = 0
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
+// Goal: program that counts adults and children in soft play centre
+
+// 1 - make a function that logs adults and children ENTERING
+  // function enter(numAdults, numChildren)
+
+
+// 2 - make a function that logs adults and children LEAVING
+  // function leave(numAdults, numChildren)
+
+  
+// 3 - make a function that returns OBJECT with 2 keys and numbers
+  // of adults and children INSIDE
+   // function occupancy()
+
+
+
+
+
+
+
+
+
+
+
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
   enter: undefined,
