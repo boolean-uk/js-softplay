715955cb0eb42dc7882007299ff15f8125aa0f64
Todd Griffin <todd.griffin61@outlook.com>
2023-03-10 16:14:19 +0000

    created enter function

--- src/soft-play.js
+++ src/soft-play.js
@@ -6,6 +6,20 @@ let children = 0
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
+const enter = (numAdults, numChildren) => {
+  if (numChildren > numAdults) {
+    return false
+  } else {
+    adults += numAdults
+    children += numChildren
+    return true
+  }
+}
+
+// console.log(`Adults, ${adults} \nChildren, ${children}`)
+// console.log(enter(2, 2))
+// console.log(`Adults, ${adults} \nChildren, ${children}`)
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
   enter: undefined,
