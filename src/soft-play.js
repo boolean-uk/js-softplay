896d24475f8788e38b1b12caaf2224dd69f0b5c1
Alistair-Henshaw <alistairhenshaw@gmail.com>
2023-03-10 16:24:37 +0000

    occupancy function created

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,10 +1,34 @@
 // do not change these lines
-let adults = 0
-let children = 0
+const adults = 0
+const children = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 // Start with the occupancy function.
+
+// function occupancy(a, c){
+//   const headCount = {adults: a, children: c}
+
+//   return headCount
+// }
+
+// function occupancy(a, c){
+//   const headCount = {
+//   adults: a,
+//   children: c}
+//   return headCount
+// }
+
+function occupancy(a, c) {
+  return { adults: a, children: c }
+}
+
+console.log(occupancy(adults, children))
+console.log(adults)
+console.log(children)
+
+// inside an object number of children and adults change
+// creates an object headCount
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
