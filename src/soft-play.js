d8bedd08cf9f732920f64dc4ef6aff55ca707c89
Lee Kelly <leeowenkelly@gmail.com>
2023-03-10 17:49:45 +0000

    completed exercises

--- src/soft-play.js
+++ src/soft-play.js
@@ -16,20 +16,26 @@ function enter(numAdultsEntered, numChildrenEntered) {
   return true
 }
 
-enter(1, 1)
-enter(2, 2)
-enter(0, 2)
-enter(4, 4)
+// enter(1, 1)
+// enter(2, 2)
+// enter(0, 2)
+// enter(4, 4)
 
 function leave(numAdultsLeft, numChildrenLeft) {
-  if (numChildrenLeft > numAdultsLeft || children < numChildrenLeft + 1 || adults < numAdultsLeft || numAdultsLeft < 0 || numChildrenLeft < 0) {
-    return false
-  }
-  adults -= numAdultsLeft
-  children -= numChildrenLeft
-  console.log("adults :", adults, "children :", children)
-  return true
-} 
+  if(numAdultsLeft === 0) {
+   return false
+ } else if(adults - numAdultsLeft < children - numChildrenLeft) {
+   return false
+ } else if(numChildrenLeft > numAdultsLeft) {
+   return false
+ } else if (adults < numAdultsLeft || children < numChildrenLeft) {
+   return false
+ } else {
+   adults -= numAdultsLeft
+   children -= numChildrenLeft
+   return true
+ }
+}
 
 leave(1, 1)
 leave(3, 3)
@@ -40,9 +46,11 @@ function occupancy() {
   return { adults: adults, children: children }
 }
 
+console.log(occupancy())
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy
 }
