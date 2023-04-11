52891a0c5ea100b78467ed476bb7616bceff48fc
ThanosPa <papanikolaoua7@gmail.com>
2023-04-11 14:53:14 +0300

    Thanasis occupandy and enter functions added

--- src/soft-play.js
+++ src/soft-play.js
@@ -5,10 +5,24 @@ let children = 0
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 // Start with the occupancy function.
-
+function occupancy(){
+  return {adults:adults, children:children}
+}
+function enter(numAdults, numChildren){
+  if(numAdults>=numChildren){
+    adults += numAdults
+    children += numChildren
+    return true
+  }
+  return false
+}
+console.log(occupancy())
+console.log(enter(0,1))
+console.log(enter(2,1))
+console.log(occupancy())
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
+  enter: enter,
   leave: undefined,
-  occupancy: undefined
+  occupancy: occupancy
 }
