0255810c2bf5baad361c011c1e5940c4cf54ff26
Rose Breedveld <rosebreedveld@hotmail.com>
2023-04-11 08:29:57 -0400

    made function occupancy

--- src/soft-play.js
+++ src/soft-play.js
@@ -2,9 +2,20 @@
 let adults = 0
 let children = 0
 
+
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 // Start with the occupancy function.
+function occupancy() {
+  const softPlay = {
+  adults: 0,
+  children: 0
+}
+softPlay.adults = adults
+softPlay.children = children
+return softPlay
+}
+console.log(occupancy())
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
