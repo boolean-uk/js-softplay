09cd5a18c99ea63bcd17395e5ebe4226178ee5e6
dtsimaras <tsimaras.dimitrios@gmail.com>
2023-04-11 16:18:35 +0300

    Soft-play funcs

--- src/soft-play.js
+++ src/soft-play.js
@@ -6,9 +6,42 @@ let children = 0
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
+function enter(numAdults, numChildren) {
+  const isAccompanied = numAdults >= numChildren
+  if (isAccompanied) {
+    adults += numAdults
+    children += numChildren
+  }
+  return isAccompanied
+}
+
+function leave(numAdults, numChildren) {
+  const isAccompanied = numAdults >= numChildren
+
+  const haveEnoughAdultsInSoftplay =
+    adults - numAdults >= children - numChildren
+
+  const arbitaryStatement =
+    adults - numAdults >= 0 && children - numChildren >= 0
+
+  const condition =
+    isAccompanied && haveEnoughAdultsInSoftplay && arbitaryStatement
+
+  if (condition) {
+    adults -= numAdults
+    children -= numChildren
+  }
+
+  return condition
+}
+
+function occupancy() {
+  return { adults, children }
+}
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+  enter,
+  leave,
+  occupancy
 }
