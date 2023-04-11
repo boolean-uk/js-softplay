cafd6ee7baf9e422917e29c17a6d495e58bc27e9
joeri luteyn <joeriluteyn@gmail.com>
2023-04-11 13:35:46 +0200

    extensions done

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,6 +1,9 @@
 // do not change these lines
 let adults = 0
 let children = 0
+let adultEnterd = 0
+let childrenEnterd = 0
+
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
@@ -20,6 +23,8 @@ const enter = (numAdults, numChildren) => {
   if(numChildren / numAdults <= 1){ 
     adults += numAdults;
     children += numChildren
+    adultEnterd += numAdults
+    childrenEnterd += numChildren
     return true
   }
 
@@ -43,10 +48,18 @@ const leave = (numAdults, numChildren) => {
 
 }
 
+const total = () => {
+  return {
+  adults: adultEnterd,
+  children: childrenEnterd
+  }
+}
+
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
   enter: enter,
   leave: leave,
-  occupancy: occupancy
+  occupancy: occupancy,
+  total: total
 }
