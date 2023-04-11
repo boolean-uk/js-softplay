1891eb3528f1f93bbc4079d5d272e03a344154f8
John Poikilidis <akiskawi@gmail.com>
2023-04-11 15:50:00 +0300

    enter function completed

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,14 +1,30 @@
 // do not change these lines
 let adults = 0
 let children = 0
+let totalAdults = 0
+let totalChildren = 0
 
+function enter(numAdults, numChildren) {
+  if (numAdults < numChildren) {
+    return false
+  }
+  totalAdults += numAdults
+  totalChildren += numChildren
+  adults += numAdults
+  children += numChildren
+  return true
+}
+function leave(numAdults, numChildren) {}
+function occupancy() {}
+function total() {}
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy,
+  total: total
 }
