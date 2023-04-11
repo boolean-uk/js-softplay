6ca80584365f2fa0287c6a9e81854c7e9eed1636
Rose <rosevandergiessen@proton.me>
2023-04-11 14:24:22 +0200

    occupancy function and enter function added

--- src/soft-play.js
+++ src/soft-play.js
@@ -4,7 +4,28 @@ let children = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
+
 // Start with the occupancy function.
+function occupancy() {
+  return { adults: adults, children: children }
+}
+
+// console.log(occupancy())
+// Enter function
+// Every child entering the soft play center is accompanied by at least 1 adult.
+function enter(numAdults, numChildren) {
+  if (numChildren > numAdults) {
+    console.log('error')
+    return false
+  } else {
+    adults += numAdults
+    children += numChildren
+    return true
+  }
+}
+
+// Leave function
+
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
