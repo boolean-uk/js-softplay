991408a4cc79b7bacf93f1ee222f2043fafb60bb
Sebastian Andrisan <sebyyyandr@gmail.com>
2023-04-11 15:01:24 +0300

    leave solved

--- src/soft-play.js
+++ src/soft-play.js
@@ -13,7 +13,7 @@ function occupancy() {
   }
 }
 
-function enter(numAdults, numChildren){
+function enter(numAdults, numChildren) {
   if (numAdults >= numChildren) {
     adults += numAdults
     children += numChildren
@@ -23,9 +23,26 @@ function enter(numAdults, numChildren){
   }
 }
 
+function leave(numAdults, numChildren) {
+  if (numAdults < numChildren) {
+    return false
+  }
+  const current = occupancy()
+  if (current.adults - numAdults < current.children - numChildren) {
+    return false
+  }
+  if (current.adults + current.children < numAdults + numChildren) {
+    return false
+  }
+
+  adults -= numAdults
+  children -= numChildren
+  return true
+}
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy
 }
