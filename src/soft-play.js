c0d2de7fa8256cb9e8afadf5b7d723eb24e1491b
Stavros <spiliopoulos.stavros@hotmail.com>
2023-04-11 14:37:18 +0300

    Enter function commited

--- src/soft-play.js
+++ src/soft-play.js
@@ -6,6 +6,23 @@ let children = 0
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
+function occupancy() {
+  return {
+    adults: adults,
+    children: children
+  }
+}
+
+function enter(numAdults, numChildren){
+  if (numAdults >= numChildren) {
+    adults += numAdults
+    children += numChildren
+    return true
+  } else {
+    return false
+  }
+}
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
   enter: undefined,
