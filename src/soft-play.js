37efaf23cf6e0d8ab6b87f0475ddd34787618055
Michał Klukowski <michal.klukowski@talan.com>
2023-07-20 10:34:10 +0200

    Core done

--- src/soft-play.js
+++ src/soft-play.js
@@ -6,9 +6,42 @@ let children = 0
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
+function occupancy() {
+  return {
+    adults: adults,
+    children: children
+  }
+}
+
+function enter(numAdults, numChildren) {
+  if(numChildren > numAdults)
+    return false
+
+  adults += numAdults
+  children += numChildren
+  
+  return true
+}
+
+function leave(numAdults, numChildren) {
+  if(numChildren > numAdults)
+    return false
+
+  if(children - numChildren > adults - numAdults)
+    return false
+
+  if(children - numChildren < 0 ||  adults - numAdults < 0)
+    return false
+
+  adults -= numAdults
+  children -= numChildren
+
+  return true
+}
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy
 }
