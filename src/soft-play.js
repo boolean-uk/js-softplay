b9d229b82232a986e39182a8485b25addf5e427b
Katarzyna Oleksiewicz <katarzyna.oleksiewicz@talan.com>
2023-07-20 10:39:17 +0200

    Extension finished

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,7 +1,8 @@
 // do not change these lines
 let adults = 0
 let children = 0
-
+let totalAdults=0
+let totalChildren=0
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 // Start with the occupancy function.
@@ -9,6 +10,8 @@ function enter(numAdults, numChildren){
   if (numAdults >= numChildren){
       adults += numAdults
       children += numChildren
+      totalAdults+=numAdults
+      totalChildren+=numChildren
       return true
   }
   return false
@@ -26,11 +29,14 @@ function leave(numAdults, numChildren){
 function occupancy(){
   return {adults: adults, children: children}
 }
-
+function total(){
+  return {adults: totalAdults, children: totalChildren}
+}
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
   enter: enter,
   leave: leave,
-  occupancy: occupancy
+  occupancy: occupancy,
+  total
 }
