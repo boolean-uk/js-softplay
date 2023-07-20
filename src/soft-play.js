aba65f6d5787d85abdf1aced43ec7e4ed35022a6
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-20 10:30:57 +0200

    Complete core exercise

--- src/soft-play.js
+++ src/soft-play.js
@@ -2,13 +2,42 @@
 let adults = 0
 let children = 0
 
+const isEnoughAdults = (a, c) => a >= c
+
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 // Start with the occupancy function.
+const enter = (numAdults, numChildren) => {
+  if (!isEnoughAdults(numAdults, numChildren)) {
+    return false
+  }
+  adults += numAdults
+  children += numChildren
+  return true
+}
+
+const leave = (numAdults, numChildren) => {
+  if (
+    !isEnoughAdults(numAdults, numChildren) ||
+    !isEnoughAdults(adults - numAdults, children - numChildren)
+  ) {
+    return false
+  }
+  adults -= numAdults
+  children -= numChildren
+  return true
+}
+
+const occupancy = () => {
+  return {
+    adults: adults,
+    children: children
+  }
+}
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+  enter,
+  leave,
+  occupancy
 }
