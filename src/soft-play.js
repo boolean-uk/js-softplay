810593bcbd48711ce22f2149150a70ae25de805e
Anna Sperkach <annsperkach@gmail.com>
2023-07-20 11:59:19 +0300

    Extension requirement

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,6 +1,8 @@
 // do not change these lines
 let adults = 0
 let children = 0
+let totalAdults = 0
+let totalChildren = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
@@ -11,6 +13,8 @@ function enter(numAdults, numChildren) {
   }
   adults += numAdults
   children += numChildren
+  totalAdults += numAdults
+  totalChildren += numChildren
   return true
 }
 
@@ -31,12 +35,20 @@ function occupancy() {
   return {
     adults,
     children
-  };
+  }
+}
+
+function total() {
+  return {
+    adults : totalAdults,
+    children : totalChildren
+  }
 }
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
   enter: enter,
   leave: leave,
-  occupancy: occupancy
+  occupancy: occupancy,
+  total: total
 }
