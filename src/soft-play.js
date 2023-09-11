c245e9004ecc5718b0acc236ddfd16a4fce1d3a5
aayushlama4008 <aayushlama4008@gmail.com>
2023-09-11 21:47:09 +0200

    solved

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,10 +1,10 @@
 // do not change these lines
-let adults = 0
-let children = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 // Start with the occupancy function.
+let adults = 0
+let children = 0
 let totalAdults = 0
 let totalChildren = 0
 const total = () => {
