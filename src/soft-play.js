db217bdf9217ed2bfc85feb96f5fed82a3daf8ce
Rebecca Noy <rebecca.noy@gmx.de>
2023-09-11 16:34:40 +0200

    extension is somehow not going through

--- src/soft-play.js
+++ src/soft-play.js
@@ -11,15 +11,31 @@ let numAdults = 0;
 let numChildren = 0;
 
 // here is what the object of the softballCenter looks like
-softballCenter = {
+let softballCenter = {
   "adults": numAdults,
   "children": numChildren,
 }
 
+// here's the tracker that tracks the total
+let softballCenterTotal = {
+  "adults": 0,
+  "children": 0,
+}
+
 // function that basically only prints out the current state of the softball-Center-object
 const occupancy = (() => softballCenter)
-console.log(occupancy())
+const total = (() => softballCenterTotal)
 
+//adding shit so i can easily reset them – not
+const resetOccupancy = (() => {
+  softballCenter.adults = 0,
+  softballCenter.children = 0
+})
+
+const resetTotalCount = (() => {
+  softballCenterTotal.adults = 0,
+  softballCenterTotal.children = 0
+})
 
 // softballCenter.adults += 1
 // console.log("occupancy after adding one adult manually", occupancy())
@@ -28,6 +44,8 @@ enter = (num1, num2) => {
   if (num1 >= num2) {
     softballCenter.adults += num1
     softballCenter.children += num2
+    softballCenterTotal.adults += num1
+    softballCenterTotal.children += num2
     return true
   } else {
     return false
@@ -36,8 +54,8 @@ enter = (num1, num2) => {
 
 // what if we add 2 adults and 1 child
 enter(2, 1)
-console.log("softball center after using the variable that stores the function to add a number of adults", occupancy())
-console.log(occupancy())
+// console.log("softball center after using the variable that stores the function to add a number of adults", occupancy())
+// console.log("occupancy:", occupancy(), "total:", totalCount())
 
 leave = (num1, num2) => {
   if (num1 >= num2 && softballCenter.adults - num1 >= softballCenter.children - num2) {
@@ -50,19 +68,22 @@ leave = (num1, num2) => {
 }
 
 leave(1, 0)
-console.log("softball center after having one adult leaving", occupancy())
+// console.log("softball center after having one adult leaving")
+// console.log("occupancy:", occupancy(), "total:", totalCount())
 
 // try having the child leave on its own
 leave(0, 1)
-console.log("softball center after the child tried to leave", occupancy())
+// console.log("softball center after the child tried to leave")
+// console.log("occupancy:", occupancy(), "total:", totalCount())
 
 leave(1,1)
-console.log("after the child leaves with an adult", occupancy())
-// softballCenter 
+// console.log("after the child leaves with an adult")
+// console.log("occupancy:", occupancy(), "total:", totalCount())
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
   enter: enter,
   leave: leave,
   occupancy: occupancy,
+  total: total,
 }
