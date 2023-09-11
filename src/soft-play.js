950bd36cf2629805b73efa4581d8542b95695990
Lukas Dembicki <lad_777@hotmail.com>
2023-09-11 17:18:36 +0100

    why cant a mom leave with her 2 kids!!!, also done exercise

--- src/soft-play.js
+++ src/soft-play.js
@@ -5,10 +5,57 @@ let children = 0
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 // Start with the occupancy function.
+function occupancy() {
+  return { adults: adults, children: children }
+}
+
+function enter(numAdults, numChildren) {
+  if (numAdults < numChildren) {
+    return false
+  } else {
+    adults = numAdults + adults
+    children = numChildren + children
+  }
+  return true
+}
+// enter(10, 10)
+// console.log(occupancy())
+
+function leave(numAdults, numChildren) {
+  const potentialChildren = children - numChildren
+  const potentialAdults = adults - numAdults
+  if (
+    numAdults < numChildren ||
+    potentialAdults < potentialChildren ||
+    potentialChildren < 0 ||
+    potentialAdults < 0
+  ) {
+    // console.log('Failed, No adults, or more kids inside')
+    return false
+  }
+  adults = adults - numAdults
+  children = children - numChildren
+  return true
+}
+
+// leave(1, 2)
+// console.log(occupancy())
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy
 }
+
+// function leave(numAdults, numChildren) {
+//   const futureAdults = adults - numAdults
+//   const futureChildren = children - numChildren
+//   if (numAdults < 1 || futureAdults < futureChildren || futureAdults < 0 || futureChildren < 0) {
+//     console.log('returning false')
+//     return false
+//   }
+//   adults -= numAdults
+//   children -= numChildren
+//   return true
+// }
