4c4cbf9e2a59acc982d274c34d0d4b848c959aec
sohaibahsanmehmood <tellnet23@hotmail.it>
2023-09-11 14:38:51 +0100

    completed

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,14 +1,48 @@
 // do not change these lines
-let adults = 0
-let children = 0
+let adults = 3
+let children = 4
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
+// increment adult number function
+function enter(person) {
+  if (person === 'adult') {
+    adults++
+    console.log(
+      `An adult has entered. Adults: ${adults}, Children: ${children}`
+    )
+  } else if (person === 'child') {
+    children++
+  }
+  console.log(`A child has entered. Adults: ${adults}, Children: ${children}`)
+}
+
+// decrease leave function
+
+function leave(person) {
+  if (person === 'adult') {
+    adults--
+  }
+  console.log(`An adult has left. Adults: ${adults}, Children: ${children}`)
+
+  if (person === 'child') {
+    children--
+  }
+  console.log(`A child has left. Adults: ${adults}, Children: ${children}`)
+}
+
+// number of people
+
+function occupancy() {
+  return adults + children
+}
+console.log(`Current Occupancy - Adults: ${adults}, Children: ${children}`)
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+  enter,
+  leave,
+  occupancy
 }
