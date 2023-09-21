d1142dc013b8f3e96831e0b517dfdf93d4b528c7
Daniel <dadefataki@gmail.com>
2023-09-21 12:10:17 +0200

    some changes

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,6 +1,6 @@
 // do not change these lines
-let adults = 25
-let children = 20
+let adults = 0
+let children = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
