839d4174d59746b64059ea10bf6fb503063a5a2b
Faiza <faraz_ul_islam@yahoo.com>
2023-11-21 16:20:48 +0000

    js softplay

--- src/soft-play.js
+++ src/soft-play.js
@@ -6,9 +6,63 @@ let children = 0
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
-// TODO: Change the undefined values below to the name of your functions
-module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+let numAdults = 0
+let numChildren = 0
+
+let counter = 0
+
+function incrementCounter(numAdults, numChildren) {
+  adults = adults + numAdults
+  children = children + numChildren
+  counter++
 }
+
+function decrementCounter(numAdults, numChildren) {
+  adults = adults - numAdults
+  children = children - numChildren
+  counter--
+}
+
+function occupancy() {
+  return {
+    children: children,
+    adults: adults,
+  }
+}
+
+  function enter(numAdults, numChildren) {
+
+    if (numAdults >= 1 && numChildren > 0) {
+      incrementCounter(numAdults, numChildren)
+      console.log(occupancy(adults, children))
+      return true
+    } else {
+      return false
+    }
+
+  }
+  console.log(enter(4, 2))
+
+
+  function leave(numAdults, numChildren) {
+
+    if (numAdults >= 1 && numChildren <= 1) {
+
+      decrementCounter(numAdults, numChildren);
+       //console.log(occupancy(adults, children))
+       console.log(`Number of adults: ${adults}, Number of children: ${children}`);
+      return true
+    } else {
+      return false
+    }
+  }
+
+  console.log(leave(2, 1))
+
+
+  // TODO: Change the undefined values below to the name of your functions
+  module.exports = {
+    enter: undefined,
+    leave: undefined,
+    occupancy: undefined
+  }
