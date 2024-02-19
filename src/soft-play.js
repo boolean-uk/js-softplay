a8a94b86ababea206b99df6446d27bf9802b4d39
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-02-19 11:03:33 +0100

    Finish core tasks

--- src/soft-play.js
+++ src/soft-play.js
@@ -2,13 +2,39 @@
 let adults = 0
 let children = 0
 
-// TODO: Write your functions in the below section. Your functions should update
-// the adults and children variables defined above.
-// Start with the occupancy function.
+function enter(numAdults, numChildren) {
+  if (numChildren > numAdults) {
+    return false
+  }
+  adults += numAdults
+  children += numChildren
+  return true
+}
 
-// TODO: Change the undefined values below to the name of your functions
+function leave(numAdults, numChildren) {
+  if (
+    numChildren > numAdults ||
+    adults - numAdults < 0 ||
+    children - numChildren < 0
+  ) {
+    return false
+  }
+
+  if (adults - numAdults < children - numChildren) {
+    return false
+  }
+
+  adults -= numAdults
+  children -= numChildren
+  return true
+}
+
+function occupancy() {
+  return { adults: adults, children: children }
+}
+
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy
 }
