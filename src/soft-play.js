082c329bc72df5b181fe390cb22a5ecc6d03eac9
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-02-19 11:12:36 +0100

    Add reflection

--- src/soft-play.js
+++ src/soft-play.js
@@ -41,6 +41,16 @@ function total() {
   return { adults: totalAdults, children: totalChildren }
 }
 
+/* Reflection
+If we want to keep track of multiple soft play centers at the same time,
+we would want to create a SoftPlayCenter class. This way we could have each
+instantiated SoftPlayCenter keep track of their respective adult/children counter.
+
+If we want different rules for each of the centers, we could have an ISoftPlayCenter interface (or an abstract class)
+that gives us a fundamental set of methods to implement, but the methods may vary in how
+the rules are implemented.
+*/
+
 module.exports = {
   enter: enter,
   leave: leave,
