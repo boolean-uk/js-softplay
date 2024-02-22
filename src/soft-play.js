79a262dc3d66671626c04b973c641e19131e460d
Alexander Niklasson <alexander.niklasson@live.com>
2024-02-22 11:11:38 +0100

    core and extension complete

--- src/soft-play.js
+++ src/soft-play.js
@@ -6,9 +6,86 @@ let children = 0
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
+let adultsTotal = 0;
+let childrenTotal = 0;
+
+function enter (adultsNumber, childrenNumber) {
+  
+  if(adults === 0 && children > 0) {
+    return false;
+  }
+  if(childrenNumber > adultsNumber){
+    return false;
+  }
+  adults += adultsNumber
+  children+= childrenNumber
+
+  adultsTotal += adultsNumber;
+  childrenTotal += childrenNumber;
+  return true;
+}
+
+function leave (adultsNumber, childrenNumber) {
+  if(adultsNumber === 1 && adults - adultsNumber > 1 && children >= adults - adultsNumber){
+    adults -= adultsNumber
+      children -= childrenNumber
+      return true;
+  }
+  if(childrenNumber === 1 && adultsNumber == 1 && adults > 0 && children > 0)  {
+      adults -= adultsNumber
+      children -= childrenNumber
+      return true;
+  }
+
+  if(children === 1 && adults === 1 && adultsNumber > 0 && childrenNumber === 0)  {
+    return false;
+  }
+  
+  if(adults - adultsNumber === 0 && children - childrenNumber === 0) {
+      adults -= adultsNumber
+      children -= childrenNumber
+      return true;
+  }
+  
+  if(adultsNumber > adults || childrenNumber > children) {
+    return false;
+  }
+  if(adultsNumber === 0 && childrenNumber > 0) {
+    return false;
+  }
+  
+  if(adultsNumber > 0 && children > 1) {
+    return false;
+  }
+  
+  
+  
+  adults -= adultsNumber
+  children -= childrenNumber
+  return true;
+}
+
+function occupancy () {
+  console.log(adults, children + " occupancy")
+  return {
+    adults: adults,
+    children: children
+  }
+}
+
+function total (){
+  console.log(adultsTotal, childrenTotal + " total")
+  return {
+    adults: adultsTotal,
+    children: childrenTotal
+  }
+}
+
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy,
+  total: total
 }
