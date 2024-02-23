9e22724747778bb810582e8aa220a4a52772393c
Kyle Vann <kyle.vann1996@icloud.com>
2024-02-23 21:19:08 +0000

    completely messed it up

--- src/soft-play.js
+++ src/soft-play.js
@@ -6,9 +6,38 @@ let children = 0
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
+const enter = (numAdults, numChildren) => {
+  adults += numAdults;
+  children += numChildren;
+  return true;
+};
+
+const leave = (numAdults, numChildren) => {
+  if (children >= numChildren) {
+    adults -= numAdults;
+    children -= numChildren;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const occupancy = () => {
+  return { adults, children };
+};
+
+// Example usage:
+const result = enter(2, 3);
+console.log(result);
+console.log('Current Occupancy:', occupancy());
+
+const result1 = leave(1, 1);
+console.log(result1);
+console.log('Current Occupancy:', occupancy());
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
-}
+  enter,
+  leave,
+  occupancy
+};
