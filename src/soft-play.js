56a8395d43b885c99eda97b86afecfd8c3f4eb66
Shaun Harris <shaun@shaunwilliamharris.com>
2024-02-27 11:14:21 +0000

    JS_SP_PASS

--- src/soft-play.js
+++ src/soft-play.js
@@ -6,9 +6,55 @@ let children = 0
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
-// TODO: Change the undefined values below to the name of your functions
+// Total in softplay area
+
+let totalAdults = 0
+let totalChildren = 0
+
+function occupancy() {
+  return {
+    adults: adults,
+    children: children
+  }
+}
+
+function enter(numAdults, numChildren) {
+  if (numChildren > numAdults) {
+    return false
+  }
+  adults += numAdults
+  children += numChildren
+
+  totalAdults += numAdults
+  totalChildren += numChildren
+
+  return true
+}
+
+function leave(numAdults, numChildren) {
+  if ((numAdults === 0) ||
+  ((adults - numAdults) <0 || (children - numChildren) <0) ||
+  ((adults - numAdults) < (children - numChildren)) ||
+  (numChildren > numAdults)) { 
+      return false
+  }
+  adults -= numAdults
+  children -= numChildren
+  return true
+}
+
+function total() {
+  return {
+    adults: totalAdults,
+    children: totalChildren
+  }
+}
+
+console.log(total())
+
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy,
+  total: total
 }
