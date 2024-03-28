009e85b1935004cf6ac38623e974b4d3a4d4b802
Azan-H <azanhussain786@gmail.com>
2024-03-28 10:55:44 +0000

    Azan Hussain

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,3 +1,5 @@
+// const { a } = require("../../js-fundamentals-functions-1/src/extensions/creating-functions-multiple-args")
+
 // do not change these lines
 let adults = 0
 let children = 0
@@ -6,9 +8,36 @@ let children = 0
 // the adults and children variables defined above.
 // Start with the occupancy function.
 
+function enter (numAdults, numChildren) {
+  if (numAdults > 0 && numAdults >= numChildren) {
+    adults += numAdults, 
+    children += numChildren
+    return true
+  } else return false
+  
+}
+
+
+
+function leave (numAdults, numChildren) {
+  if (numAdults >= numChildren && numAdults <= adults && numChildren <= children && adults - numAdults >= children - numChildren) {
+    adults -= numAdults,
+    children -= numChildren
+    return true
+  } else return false
+}
+
+
+function occupancy () {
+  console.log({adults:adults, children:children})
+  return {adults:adults, children:children}
+}
+
+
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy
 }
